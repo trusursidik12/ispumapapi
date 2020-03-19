d54e613ccc78abd72669d094b61c465077d50c67
warih suryono <warih_suryono@yahoo.com>
2020-03-19 15:14:36 +0700

    aaa

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -62,7 +62,7 @@ class Api extends RestController {
 	public function aqmStasiun_get()
 	{
 
-		$id = $this->get('id_stasiun');
+		$id = $this->get('id');
 
 		if ($id === null) {
 			$data = $this->aqmmaster_m->get_aqm_stasiun();
@@ -95,32 +95,32 @@ class Api extends RestController {
 		}
 
 		if ($data) {
+			$id_stasiuns = [];
+			foreach($data as $province){
+				$id_stasiuns[] = $province["id_stasiun"];
+			}
+
+			$worst_ispu = 0;
+			$worst_param = "";
+			$worst_stasiun_id = "";
+			$ispu = $this->aqmmaster_m->get_ispu($id_stasiuns);
+			foreach($ispu as $id_stasiun => $stasiun){
+				if($worst_ispu < $stasiun["pm25"]){ $worst_ispu = $stasiun["pm25"]; $worst_param = "pm25"; $worst_stasiun_id = $stasiun["id_stasiun"]; }
+				if($worst_ispu < $stasiun["pm10"]){ $worst_ispu = $stasiun["pm10"]; $worst_param = "pm10"; $worst_stasiun_id = $stasiun["id_stasiun"]; }
+				if($worst_ispu < $stasiun["so2"]){ $worst_ispu = $stasiun["so2"]; $worst_param = "so2"; $worst_stasiun_id = $stasiun["id_stasiun"]; }
+				if($worst_ispu < $stasiun["co"]){ $worst_ispu = $stasiun["co"]; $worst_param = "co"; $worst_stasiun_id = $stasiun["id_stasiun"]; }
+				if($worst_ispu < $stasiun["o3"]){ $worst_ispu = $stasiun["o3"]; $worst_param = "o3"; $worst_stasiun_id = $stasiun["id_stasiun"]; }
+				if($worst_ispu < $stasiun["no2"]){ $worst_ispu = $stasiun["no2"]; $worst_param = "no2"; $worst_stasiun_id = $stasiun["id_stasiun"]; }
+			}
+			
+			$category = $this->aqmmaster_m->get_category($worst_ispu);
+			$effect = $this->aqmmaster_m->get_effect($worst_ispu,$worst_param);
+			$resumes = ["worst_ispu" => $worst_ispu,"worst_param" => $worst_param,"worst_stasiun_id" => $worst_stasiun_id, "category" => $category, "effect" => $effect, "ispu" => $ispu];
+			
+			
 			$this->response([
                     'status' 	=> true,
-                    'data' 		=> $data
-                ], 200);
-		} else {
-			$this->response([
-                    'status' 	=> false,
-                    'message' 	=> 'Data Tidak Ditemukan'
-                ], 404);
-		}
-	}
-
-	public function aqmProvinceList_get()
-	{
-
-		$id = $this->get('provinsi');
-
-		if ($id === null) {
-			$data = $this->aqmmaster_m->get_aqm_province_list();
-		} else {
-			$data = $this->aqmmaster_m->get_aqm_province_list($id);		
-		}
-
-		if ($data) {
-			$this->response([
-                    'status' 	=> true,
+                    'resumes' 	=> $resumes,
                     'data' 		=> $data
                 ], 200);
 		} else {
--- application/models/Aqmmaster_m.php
+++ application/models/Aqmmaster_m.php
@@ -30,13 +30,24 @@ class Aqmmaster_m extends CI_Model
 	public function get_aqm_stasiun($id = FALSE)
 	{
 		if($id === FALSE){
-			$this->db->order_by('id', 'ASC');
+			$this->db->order_by('id', 'DESC');
 			$query = $this->db->get('aqm_stasiun');
 			return $query->result_array();
 		}
-		$query = $this->db->get_where('aqm_stasiun', array('id_stasiun' => $id));
+		$query = $this->db->get_where('aqm_stasiun', array('id' => $id));
 		return $query->row_array();
 	}
+
+	// public function get_province($id = FALSE)
+	// {
+	// 	if($id === FALSE){
+	// 		$this->db->join('aqm_stasiun', 'aqm_stasiun.id_stasiun = indoor_groups.id_stasiun');
+	// 		$query = $this->db->get('indoor_groups');
+	// 		return $query->result_array();
+	// 	}
+	// 	$query = $this->db->get_where('indoor_groups', array('id_group' => $id));
+	// 	return $query->row_array();
+	// }
 
 	public function get_aqm_province($id = FALSE)
 	{
@@ -49,22 +60,30 @@ class Aqmmaster_m extends CI_Model
 		return $query->result_array();
 	}
 
-	public function get_aqm_province_list($id = FALSE)
+	public function get_ispu($id_stasiuns)
 	{
-		if($id === FALSE){
-			$this->db->select('DISTINCT(provinsi), id_stasiun, nama, lat, lon, alamat, kota, geojson, use_internet, dbsource, old_id, xtimetimes');
-			$this->db->group_by('provinsi'); 
-			$this->db->order_by('provinsi', 'ASC');
-			$query = $this->db->get('aqm_stasiun');
-			return $query->result_array();
-		}
-		$query = $this->db->get_where('aqm_stasiun', array('provinsi' => $id));
+		if(date("H") * 1 < 15)
+			$waktu = date("Y-m-d",mktime(0,0,0,date("m"),date("d")-1));
+		else
+			$waktu = date("Y-m-d",mktime(0,0,0,date("m"),date("d")));
+		
+		$this->db->where_in('id_stasiun', $id_stasiuns);
+		$this->db->like('waktu', $waktu, 'after'); 
+		$query = $this->db->get("aqm_ispu");
 		return $query->result_array();
 	}
-
-	// public function get_higestdata()
-	// {
-	// 	$query = $this->db->query("SELECT greatest(max(so2),max(co),max(o3),max(no2),max(hc),max(voc),max(nh3)) FROM aqm_data");
-	// 	return $query->result();
-	// }
+	
+	public function get_category($ispu,$param = ""){
+		if($param != "") $this->db->where("param",$param);
+		$this->db->where("'$ispu' BETWEEN ispu_a AND ispu_b");
+		$query = $this->db->get("categories");
+		return $query->row_array()["category"];
+	}
+	
+	public function get_effect($ispu,$param){
+		$this->db->where("param",$param);
+		$this->db->where("'$ispu' BETWEEN ispu_a AND ispu_b");
+		$query = $this->db->get("categories");
+		return $query->row_array()["effect"];
+	}
 }
